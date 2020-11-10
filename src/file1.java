81caf175f3f863d6434d3074dd57419b571a6087
esma17 <milkysf@gmail.com>
2020-11-10 12:04:07 -0600

    A new value for y

--- src/file1.java
+++ src/file1.java
@@ -2,5 +2,8 @@ public class file1 {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         int i=0;
+        int y;
+        y=i+2;
+        System.out.println(i);
     }
 }
